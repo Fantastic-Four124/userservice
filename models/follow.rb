baf70a1cea95c4b0d46152ff124f393ba26fa7cb
Zhengyang Zhou <zhengyjo@brandeis.edu>
2018-04-08 18:01:27 -0400

    follow

--- models/follow.rb
+++ models/follow.rb
@@ -2,6 +2,7 @@ class Follow < ActiveRecord::Base
   include ActiveRecord::Calculations
     validates :user_id,presence:true
     validates :leader_id,presence:true
+    validates :user_id, uniqueness: { scope: :leader_id }
 
     belongs_to :user
     belongs_to :leader, class_name: 'User'
