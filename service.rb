9d4a9450e1516c29ebe5b360ceb5bd79fa80d54d
Zhengyang Zhou <zhengyjo@brandeis.edu>
2018-04-12 12:32:11 -0400

    test

--- service.rb
+++ service.rb
@@ -17,7 +17,6 @@ require_relative 'models/follow'
 configure do
     uri = URI.parse(ENV['REDISCLOUD_URL'])
     $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-    #byebug
 end
 
 set :allow_origin, '\*'
