8d317f9ca4b3478d6cd7015e0a2528195bf6b8d8
Zhengyang Zhou <zhengyjo@brandeis.edu>
2018-04-15 23:14:14 -0400

    get user

--- service.rb
+++ service.rb
@@ -131,6 +131,27 @@ post PREFIX + '/:token/logout' do
   {err: true}.to_json
 end
 
+get PREFIX + '/:token/users/:id' do
+  first_try = JSON.parse($redis.get params['id'])
+  if $redis.get params['token'])
+    if first_try
+      user = first_try
+      return first_try.to_json
+    else
+      user = User.find params['id']
+      if $redis.get params['token']) && user
+        u_hash = user.as_json
+        u_hash['leaders'] = []
+        u_hash['followers'] = []
+        user.leaders.each {|l| u_hash['leaders'].push l.id}
+        user.followers.each {|f| u_hash['followers'].push f.id}
+        return u_hash.to_json
+      end
+    end
+  end
+  {err: true}.to_json
+end
+
 post '/test/reset/all' do
   clear_all
   recreate_testuser(params)
